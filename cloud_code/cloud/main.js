277daa7255994055184c0a3ac4899e680b00059a
Lewuathe <lewuathe@me.com>
2015-11-03 22:09:10 +0900

    Obtain basic info and save Contribution class

--- cloud_code/cloud/main.js
+++ cloud_code/cloud/main.js
@@ -1,98 +1,70 @@
-var oauth = require('cloud/oauth.js');
+var OAuth = require('cloud/oauth.js');
 var sha   = require('cloud/sha1.js');
+var Twitter = require('cloud/twitter.js');
 
-var consumerKey = "LZXKM3AFHSWWy7c9EVJu90kYZ";
-var consumerSecret = "bxR8uOR1FgHLLa0g4gdbNrmu465Qb5Nsd0E14afGExmTKZ7PRV";
-
-var accessor = {
-    "consumerSecret": "3699404474-aGC4yBOzlXPCmXinDPzrPZdN3yAJb3V03HLOdls",
-    "tokenSecret": "m4AVTdJHknG0qYnkQEgVbMVhQhWfmBLbUuMmmv57R2ckP"
-};
-
-var nonce = oauth.nonce(32);
-var ts = Math.floor(new Date().getTime() / 1000);
-var timestamp = ts.toString();
-
-var params = {
-    "oauth_version": "1.0",
-    "oauth_consumer_key": consumerKey,
-    "oauth_token": consumerSecret,
-    "oauth_timestamp": timestamp,
-    "oauth_nonce": nonce,
-    "oauth_signature_method": "HMAC-SHA1"
-};
-
-var url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
-
-var message = {
-    "method": "GET",
-    "action": url,
-    "parameters": params
-};
-
-//lets create signature
-oauth.SignatureMethod.sign(message, accessor);
-var normPar = oauth.SignatureMethod.normalizeParameters(message.parameters);
-console.log("Normalized Parameters: " + normPar);
-var baseString = oauth.SignatureMethod.getBaseString(message);
-console.log("BaseString: " + baseString);
-var sig = oauth.getParameter(message.parameters, "oauth_signature") + "=";
-console.log("Non-Encode Signature: " + sig);
-var encodedSig = oauth.percentEncode(sig); //finally you got oauth signature
-console.log("Encoded Signature: " + encodedSig);
-
+var url = 'https://api.twitter.com/1.1/users/show.json';
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+  response.success("Hello  world!");
 });
 
 Parse.Cloud.define("introbank_test", function(request, response) {
     response.success("This is test");
 });
 
+/**
+ *  Collect user contribution from Twitter regularly.
+ *  Obtain authentication information from User class and
+ *  lookup their contributions such as favourites through Twitter API.
+ */
 Parse.Cloud.job("userActCollect", function(request, status) {
     console.log("===== userActCollect start =====");
+
+    // In order to lookup authentication information.
     Parse.Cloud.useMasterKey();
+
     var query = new Parse.Query(Parse.User);
-
     query.each(function(user) {
-        console.log(user);
-    }).then(function() {
-        status.success('Success');
-    }, function(error) {
-        status.error('Something error is occured');
-    });
+        if (user.get("authData") != undefined && user.get("authData").twitter != undefined) {
+            var authToken = user.get("authData").twitter.auth_token;
+            var authTokenSecret = user.get("authData").twitter.auth_token_secret;
+            var screenName = user.get("authData").twitter.screen_name;
+            var consumerKey = user.get("authData").twitter.consumer_key;
+            var consumerSecret = user.get("authData").twitter.consumer_secret;
 
-    console.log("Submit Google");
-    Parse.Cloud.httpRequest({
-        url: "http://google.com",
-    }).then(function(res) {
-        console.log('======= GOOGLE SUCCESS ======');
-    }, function(res) {
-        console.error("========== GOOGLE FAIL ========");
-    });
-
-    console.log("Submit Twitter");
-    Parse.Cloud.httpRequest({
-        method: 'GET',
-        url: url,
-        headers: {
-            "Authorization": 'OAuth oauth_consumer_key="'+consumerKey+'", oauth_nonce=' + nonce + ', oauth_signature=' + encodedSig + ', oauth_signature_method="HMAC-SHA1", oauth_timestamp=' + timestamp + ',oauth_token="'+consumerSecret+'", oauth_version="1.0"'
-        },
-        body: {
-            
+            // Access Twitter API to obtain basic info for this user.
+            Parse.Cloud.httpRequest({
+                url: url,
+                followRedirects: true,
+                headers: {
+                    "Authorization": Twitter.getOAuthSignature(url, screenName,
+                        authToken, authTokenSecret, consumerKey, consumerSecret)
+                },
+                params: {
+                    screen_name: screenName
+                }
+            }).then(function (res) {
+                // In case of request success, save his contribution
+                // into Contribution class in Parse DB.
+                Twitter.saveContribution(res.data, function(contrib) {
+                    status.success("Success");
+                }, function(error) {
+                    status.fail("Failed");
+                });
+            }, function (res) {
+                // In case of request failed
+                console.log(res.text);
+                status.fail("Failed");
+            });
         }
-    }).then(function(res) {
-        // In case of request success
-        console.log("%%%%%%%%");
-        console.log(res.text);
-    }, function(res) {
-        // In case of request failed
-        console.error("^^^^^^^ Request failed ^^^^^^^^^^^");
+    }).then(function() {
+        // Query submit succeeded
+        console.log("Query success");
+    }, function(error) {
+        // In case of query access error.
+        console.log("Query failed");
+        status.fail("Failed");
     });
-
-    console.log(request.params);
-    console.log("===== This is cloud code =====");
 });
---
+++ cloud_code/cloud/twitter.js
@@ -0,0 +1,91 @@
+var OAuth = require('cloud/oauth.js');
+var sha   = require('cloud/sha1.js');
+
+/**
+ * Create OAuth 2.0 signature for Twitter API v1.1
+ * @param url
+ * @param screenName
+ * @param authToken
+ * @param authTokenSecret
+ * @param consumerKey
+ * @param consumerSecret
+ * @returns {string}
+ */
+var getOAuthSignature = function(url, screenName, authToken, authTokenSecret,
+                                 consumerKey, consumerSecret) {
+    var nonce = OAuth.nonce(32);
+    var ts = Math.floor(new Date().getTime() / 1000);
+    var timestamp = ts.toString();
+
+    var accessor = {
+        "consumerSecret": consumerSecret,
+        "tokenSecret": authTokenSecret
+    };
+
+    var params = {
+        "screen_name": screenName,
+        "oauth_version": "1.0",
+        "oauth_consumer_key": consumerKey,
+        "oauth_token": authToken,
+        "oauth_timestamp": timestamp,
+        "oauth_nonce": nonce,
+        "oauth_signature_method": "HMAC-SHA1"
+    };
+
+    var message = {
+        "method": "GET",
+        "action": url,
+        "parameters": params
+    };
+
+    OAuth.SignatureMethod.sign(message, accessor);
+    var normPar = OAuth.SignatureMethod.normalizeParameters(message.parameters);
+    var baseString = OAuth.SignatureMethod.getBaseString(message);
+    var sig = OAuth.getParameter(message.parameters, "oauth_signature") + "=";
+    var encodedSig = OAuth.percentEncode(sig);
+
+    return 'OAuth oauth_consumer_key="'+consumerKey+'", oauth_nonce=' + nonce + ', oauth_signature=' + encodedSig + ', oauth_signature_method="HMAC-SHA1", oauth_timestamp=' + timestamp + ',oauth_token="'+authToken+'", oauth_version="1.0"'
+};
+
+// Private method
+var _parseTwitterActs = function(user) {
+    var acts = {
+        favouriteCount: user.favourites_count,
+        followerCount: user.followers_count,
+        id: user.id,
+        lang: user.lang,
+        profileImageUrl: user.profile_image_url,
+        screenName: user.screen_name
+    };
+
+    return acts;
+};
+
+/**
+ * Save the contribution of given user into Contribution class.
+ * @param user
+ * @param successCb
+ * @param failCb
+ */
+var saveContribution = function(user, successCb, failCb) {
+    var acts = _parseTwitterActs(user);
+
+    var Contribution = Parse.Object.extend("Contribution");
+    var contrib = new Contribution();
+    contrib.set("favourite_count", acts.favouriteCount);
+    contrib.set("followers_count", acts.followerCount);
+    contrib.set("lang", acts.lang);
+    contrib.set("profile_image_url", acts.profileImageUrl);
+    contrib.set("screen_name", acts.screenName);
+    contrib.save(null,{
+        success:function(contrib) {
+            successCb(contrib);
+        },
+        error:function(error) {
+            failCb(error);
+        }
+    });
+};
+
+module.exports.getOAuthSignature = getOAuthSignature;
+module.exports.saveContribution = saveContribution;
