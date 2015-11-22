13c04b0a734ca51f474b385656c2df5cdb7cd418
shoe116 <shupoppo.116@gmail.com>
2015-11-22 10:57:45 +0900

    mod default value

--- server/lib/album_media_map_data_object.py
+++ server/lib/album_media_map_data_object.py
@@ -4,7 +4,7 @@ from base_data_object import BaseDataObject
 class AlbumMediaMapDataObject(BaseDataObject):
     tableName = "AlbumMediaMap"
 
-    def insert(self, albumId, mediaId, isViewable = False):
+    def insert(self, albumId, mediaId, isViewable = True):
         album = {"__type": "Pointer", "className":"Album", "objectId":albumId}
         media =  {"__type": "Pointer", "className":"Media", "objectId":mediaId}
 
